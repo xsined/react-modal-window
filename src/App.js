ef83882728b922f4f72d4f686ff84ed4a3ae03bc
denis nash <nasheba95@gmail.com>
2023-06-11 21:31:44 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {BasicModal} from './components/basic-modal'
+
+
+function App() {
+    
+
+  return (
+    <div>
+
+   <BasicModal/>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/basic-modal.js
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import './basic-modal.css';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <div className="btnmodul">
+      <Button color ="inherit" onClick={handleOpen}>Відкрити вікно</Button>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Назва Вікна
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Основний контент та інформація.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
